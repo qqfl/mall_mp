3c3573e99ecb9c29f6edd89cc48e312c7444b264
fl <252171872@qq.com>
2020-03-25 17:39:43 +0800

    save

--- pages/detail/childCpts/colorSize/index.js
+++ pages/detail/childCpts/colorSize/index.js
@@ -7,24 +7,48 @@ Component({
   },
   data: {
     show: false,
-    color:'',
-    size:'',
-    sto:'',
-    colorIndex:null,
+    image: '',
+    sto: '',
+    color: '',
+    size: '',
+    qty: 1,
+    colorIndex: 0,
+    sizeIndex: 0,
     actStyle: 'color: #ed4566;border-color: #ed4566;'
   },
   methods: {
     showPopup() {
-      this.setData({show: true});
+      const ci = this.data.colorIndex;
+      const si = this.data.sizeIndex;
+      this.setData({show: true,});
+      this.colorSet(ci);
+      this.sizeSet(si);
     },
     onClose() {
       this.setData({show: false});
     },
     onChange(event) {
-      console.log(event.detail);
+      this.setData({qty: event.detail});
     },
     colorClick(e){
-      this.setData({colorIndex:e.currentTarget.dataset.index});
+      const index = e.currentTarget.dataset.index;
+      this.setData({colorIndex: index,});
+      this.colorSet(index);
+    },
+    sizeClick(e){
+      const index = e.currentTarget.dataset.index;
+      this.setData({sizeIndex: index,});
+      this.sizeSet(index);
+    },
+    colorSet(index){
+      this.setData({
+        sto: this.data.info.color[index].sto,
+        image: this.data.info.color[index].img,
+        color: this.data.info.color[index].col,
+      });
+    },
+    sizeSet(index){
+      this.setData({size: this.data.info.size[index]});
     }
   }
 });
---
+++ pages/detail/childCpts/moreImg/index.js
@@ -0,0 +1,11 @@
+Component({
+  externalClasses: ['parent-class'],
+  properties: {
+    info: {
+      type: Object,
+      value: {}
+    }
+  },
+  data: {},
+  methods: {}
+});
